08073a801ce976bbada5004765c6f8a8fcf2404e
Helmet <mihai.toropu@ids.software.ucv.ro>
2018-11-01 22:59:27 +0200

    CRUD Product (CREATE, READ, UPDATE, DELETE)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,39 +4,87 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
-use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/product")
+ */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="app_product")
+     * @Route("/", name="product_index", methods="GET")
      */
-    public function product(Request $request)
+    public function index(ProductRepository $productRepository): Response
     {
-        // 1) build the form
+        return $this->render('product/index.html.twig', ['products' => $productRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="product_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
-
-        // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
 
-            // 3) save the Product!
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($product);
-            $entityManager->flush();
-
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
-
-            return $this->redirectToRoute('app_product');
+            return $this->redirectToRoute('product_index');
         }
 
-        return $this->render(
-            'product/product.html.twig',
-            array('form' => $form->createView())
-        );
+        return $this->render('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="product_show", methods="GET")
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="product_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Product $product): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="product_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Product $product): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('product_index');
     }
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -1,37 +1,27 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: torop
-     * Date: 01-Nov-18
-     * Time: 8:01 PM
-     */
 
-    namespace App\Form;
+namespace App\Form;
 
-    use App\Entity\Product;
-    use phpDocumentor\Reflection\Types\Float_;
-    use Symfony\Component\Form\AbstractType;
-    use Symfony\Component\Form\Extension\Core\Type\NumberType;
-    use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-    use Symfony\Component\Form\Extension\Core\Type\TextType;
-    use Symfony\Component\Form\FormBuilderInterface;
-    use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-    class ProductType extends AbstractType
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        public function buildForm(FormBuilderInterface $builder, array $options)
-        {
-            $builder
-                ->add('price', NumberType::class)
-                ->add('name', TextType::class)
-                ->add('description', TextType::class)
-            ;
-        }
-
-        public function configureOptions(OptionsResolver $resolver)
-        {
-            $resolver->setDefaults(array(
-                'data_class' => Product::class,
-            ));
-        }
+        $builder
+            ->add('price')
+            ->add('name')
+            ->add('description')
+        ;
     }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
